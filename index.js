fe0574b4cab0bc5e895bcabff294b3dee10989e1
lucastimho <lucas.t.ho.lh@gmail.com>
2021-11-04 12:05:54 -0500

    Adds v-on:click, v-model, and v-for

---
+++ index.js
@@ -0,0 +1,36 @@
+/* global Vue */
+
+var app = new Vue({
+  el: "#app",
+  data: function () {
+    return {
+      message: "Hello from JavaScript!",
+    };
+  },
+});
+
+var app4 = new Vue({
+  el: "#app-4",
+  data: {
+    todos: [{ text: "Learn JavaScript" }, { text: "Learn Vue" }, { text: "Build something awesome" }],
+  },
+});
+
+var app5 = new Vue({
+  el: "#app-5",
+  data: {
+    message: "Here is some Javascript!",
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split("").reverse().join("");
+    },
+  },
+});
+
+var app6 = new Vue({
+  el: "#app-6",
+  data: {
+    message: "Hello Vue!",
+  },
+});
